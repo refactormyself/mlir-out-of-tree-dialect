6acf0d4964552f86a25ec70480387b2435b15d49
Saheed <refactormyself@gmail.com>
2022-05-23 19:11:04 +0200

    Add the build script

---
+++ toy/Ch1/build-proj.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+CURR_DIR=$PWD
+BUILD_DIR="/DATA/DEV/Compilers/llvm/build"
+LLVM_INSTALL="/DATA/DEV/Compilers/llvm/install"
+APP_INSTALL="$CURR_DIR/bin"
+
+mkdir $APP_INSTALL
+rm -rf build
+mkdir build
+cd build
+
+export CC=$LLVM_INSTALL/bin/clang
+export CXX=$LLVM_INSTALL/bin/clang++
+
+$CXX --version
+
+cmake -G Ninja .. \
+	-DMLIR_DIR=$LLVM_INSTALL/lib/cmake/mlir \
+    -DCMAKE_INSTALL_PREFIX=$APP_INSTALL 
+	# -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit 
+
+cmake --build . --target install
+
+cd $CURR_DIR
