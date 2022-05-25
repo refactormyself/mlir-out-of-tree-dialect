4dde499d0bbaff32e8c090148b446c2064b1850c
Saheed <refactormyself@gmail.com>
2022-05-25 20:33:52 +0200

    [CHPT-4] Move TableGen files to include and update Cmakelist

--- toy/Ch4/mlir/ToyCombine.cpp
+++ toy/Ch4/mlir/ToyCombine.cpp
@@ -20,7 +20,7 @@ using namespace toy;
 
 namespace {
 /// Include the patterns defined in the Declarative Rewrite framework.
-#include "ToyCombine.inc"
+#include "toy/ToyCombine.inc"
 } // namespace
 
 /// This is an example of a c++ rewrite pattern for the TransposeOp. It
